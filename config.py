bd642db2ebfe5371b24aad6294b603162549fa97
lawvs <18554747+lawvs@users.noreply.github.com>
2017-09-05 12:33:20 +0800

    添加pycurl检测模块 python3兼容

--- config.py
+++ config.py
@@ -6,7 +6,7 @@
 封装ConfigParser
 从固定路径读取配置文件
 
-配置文件路径CONFIG_PATH="config.ini"
+配置文件路径CONFIG_PATH
 更改值后需调用save保存
 
 常用函数
@@ -22,15 +22,21 @@ config.save()  # 保存配置文件
 创建时间: 2017-9-4 11:37:39
 '''
 
-import ConfigParser
 import logging
 import sys
-
+if sys.version < "3":  # python2
+    import ConfigParser
+else:  # python3
+    import configparser
 # 配置文件路径
 CONFIG_PATH="config.ini"
 
 # 创建ConfigParser实例
-config = ConfigParser.ConfigParser()
+if sys.version < "3":  # python2
+    config=ConfigParser.ConfigParser()
+else:  # python3
+    config=configparser.ConfigParser()
+
 # 读取配置文件
 config.read(CONFIG_PATH)
 
@@ -39,18 +45,32 @@ def save(self):
     file=open(CONFIG_PATH, "w")
     self.write(file)
     # config.write(sys.stdout)
-    logging.debug("保存配置文件 " + CONFIG_PATH)
+    logging.debug(u"保存配置文件 " + CONFIG_PATH)
+    return
 
 # 动态添加save方法
 from types import MethodType
-config.save = MethodType(save, config, ConfigParser)
+config.save = MethodType(save, config)
+
+# 设置配置文件
+def set_file(self, filename):
+    global CONFIG_PATH
+    CONFIG_PATH=filename
+    self.read(CONFIG_PATH)
+    logging.debug(u"重新加载配置文件 " + CONFIG_PATH)
+    return
+
+# 动态添加set_file方法
+from types import MethodType
+config.set_file = MethodType(set_file, config)
+
 
 #初始化日志
 def initLog():
     import logging.config
-    config_path=config.get( 'log', 'config_path')
+    config_path=config.get( "log", "config_path")
     logging.config.fileConfig(config_path)
-    logging.debug("加载日志配置文件 "+config_path)
+    logging.debug(u"加载日志配置 "+config_path)
     return
 
 #主函数 TEST
@@ -60,11 +80,13 @@ def main():
 
     #TEST
     print("config测试")
+    config.set_file("config.ini")
+    print(CONFIG_PATH)
     section = 'defaults'
     print(config.sections())
-    config.set( section, 'config_path', 'logconf.ini')
-    print("defaults:"),
-    print(config.get( 'defaults', 'test'))
+    config.set( "log", "config_path", "logging.ini")
+    print("test:"),
+    print(config.get( "defaults", "test"))
     config.set(section,"test","test2")
     #config.add_section(section)
     config.save()
--- main.py
+++ main.py
@@ -5,26 +5,33 @@
 单机服务质量监控脚本
 创建时间: 2017-9-3 14:20:21
 '''
+from __future__ import unicode_literals
 from config import config
 import logging
+import sys
+from curl_webSev import curl_webSev
 
 def monitor():
-
+    url="www.github.com"
+    logging.info("开始监测 " + url)
+    curl_webSev(url)
+    logging.info("监测完成 " + url)
     return
 
 #初始化日志
 def initLog():
     import logging.config
     # 日志配置文件位置
-    config_path=config.get( 'log', 'config_path')
+    config_path=config.get( "log", "config_path")
     logging.config.fileConfig(config_path)
-    logging.debug("加载日志配置文件 " + config_path)
+    logging.debug("加载日志配置 " + config_path)
     return
 
 #主函数
 def main():
     initLog()
     monitor()
+    print(config.get( 'defaults', 'test'))
     return
 
 #start
